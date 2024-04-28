7e6b1c83ae69a833743ee2ecdc2735f2cb25573f
Mosier <sp24816b8bf0@rpi-buster-armhf>
2024-04-28 23:22:27 +0000

    remove hidden characters

--- testEncrypt.s
+++ testEncrypt.s
@@ -3,19 +3,19 @@
 # Date: 4/23/24
 # Purpose: Tests Encrypt in RSALib
 #
- 
+
 .global main
 .text
 main:
-    # Push to stack
-    SUB sp, #4
-    STR lr, [sp]
- 
-    BL encrypt
- 
-    # Return from stack
-    LDR lr, [sp]
-    ADD sp, #4
-    MOV pc, lr
- 
- 
+    # Push to stack
+    SUB sp, #4
+    STR lr, [sp]
+
+    BL encrypt
+    
+    # Return from stack
+    LDR lr, [sp]
+    ADD sp, #4
+    MOV pc, lr
+
+
